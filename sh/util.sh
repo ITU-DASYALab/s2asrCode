5a9d862af1913ed69460888975bc91b1b27f61f1
Sebastian Baunsgaard <sebab@itu.dk>
2019-12-18 10:59:49 +0100

    Removed unnecessary sh scripts

--- sh/util.sh
+++
@@ -1,7 +0,0 @@
-
-
-
-nvidia-smi --format=csv,noheader --query-gpu=power.draw,utilization.gpu,utilization.memory,temperature.gpu
-ps -eo pcpu,pmem | sort -k 1 -r | head -2
-date +%s%N | cut -b1-13
-
